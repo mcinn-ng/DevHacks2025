4c7f26f89fb27774c0732b2541c52d7278c8ce8f
theCYBRIX <102550532+theCYBRIX@users.noreply.github.com>
2025-02-21 15:53:56 -0600

    fix: ensured jump component only works on authority

--- devhacks2025/components/jump_component.gd
+++ devhacks2025/components/jump_component.gd
@@ -9,7 +9,9 @@ const DEFAULT_JUMP_VELOCITY = -400.0
 @export var double_jump : bool = false
 @export var player : CharacterBody2D
 
+
 var _double_jump_used : bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,7 +21,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
+	if not is_multiplayer_authority():
+		return
+	
 	# Handle jump.
 	var on_floor := player.is_on_floor()
 	if Input.is_action_just_pressed("ui_accept") and (on_floor or (double_jump and not _double_jump_used)):
