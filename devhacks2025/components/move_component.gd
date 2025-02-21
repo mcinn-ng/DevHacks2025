3b65f3eb03436a24c1045058c1ef82117ca09ce7
theCYBRIX <102550532+theCYBRIX@users.noreply.github.com>
2025-02-21 16:18:11 -0600

    refactor: moved player movement into it's own component

---
+++ devhacks2025/components/move_component.gd
@@ -0,0 +1,26 @@
+class_name MoveComponent
+extends Node
+
+
+const DEFAULT_SPEED : float = 300.0
+
+
+@export var speed : float = DEFAULT_SPEED
+@export var player : CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not player:
+		player = get_parent()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		player.velocity.x = direction * speed
+	else:
+		player.velocity.x = move_toward(player.velocity.x, 0, speed)
