4855656b2284b483f5bef88dd65a341f02c42223
tsuki0272 <email4alem@gmail.com>
2025-02-22 01:57:45 -0600

    basic crawl ability and moving platform

--- devhacks2025/scripts/player.gd
+++ devhacks2025/scripts/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
+var normal_size = Vector2(1.5, 1.5)
+var small_size = Vector2(1.5, 0.75)
 
-func _enter_tree() -> void:
-	set_multiplayer_authority(name.to_int())
-
+func shrink():
+	if Input.is_action_pressed("Crawl"):
+		scale = small_size
+	else:
+		scale = normal_size
 
 func _physics_process(delta: float) -> void:
-	pass
+	shrink()
