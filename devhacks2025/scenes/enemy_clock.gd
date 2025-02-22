18964f5abcccb979a6dfa047d37248ae62576958
KarinP03 <k.plavin03@gmail.com>
2025-02-22 12:41:36 -0600

    Made the guy move

---
+++ devhacks2025/scenes/enemy_clock.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	path_follow_2d.progress += 100 *delta
