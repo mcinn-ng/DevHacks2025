3c3fef1f37957e66484c81d4b38517b164025289
theCYBRIX <102550532+theCYBRIX@users.noreply.github.com>
2025-02-24 01:45:54 -0600

    refactor: refactored shrink component

--- devhacks2025/components/shrink_component.gd
+++ devhacks2025/components/shrink_component.gd
@@ -2,12 +2,12 @@ class_name ShrinkComponent
 extends Node 
 
 
-@export var player : CharacterBody2D
+@export var player : Player : set = set_player
 
-
-var normal_size = Vector2(1.5, 1.5)
-var small_size = Vector2(1.5, 0.75)
-
+var normal_size : Vector2
+var small_size := Vector2(22, 8)
+var hitbox_offset := Vector2(0, -1)
+var small : bool = false : set = set_small
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,14 +15,32 @@ func _ready() -> void:
 		player = get_parent()
 
 
-func _physics_process(_delta: float) -> void:
-	shrink()
-	pass
-
-func shrink():
+func _input(event: InputEvent) -> void:
 	if !is_multiplayer_authority():
 		return
-	if Input.is_action_pressed("Crawl"):
-		player.scale = small_size
+	if event.is_action("Crawl"):
+		set_small.rpc(event.is_pressed())
+
+
+func set_player(node : Player) -> void:
+	player = node
+	if player:
+		normal_size = player.collision_shape_2d.shape.size
+
+@rpc("authority", "reliable", "call_local")
+func set_small(enabled : bool) -> void:
+	if small == enabled:
+		return
+	
+	small = enabled
+	
+	if small:
+		player.collision_shape_2d.shape.size = small_size
+		player.collision_shape_2d.position += hitbox_offset
+		player.body_sprite.play("Shrink")
+		player.shell_sprite.play("Shrink")
 	else:
-		player.scale = normal_size
+		player.collision_shape_2d.shape.size = normal_size
+		player.collision_shape_2d.position -= hitbox_offset
+		player.body_sprite.play("Snail")
+		player.shell_sprite.play("default")
